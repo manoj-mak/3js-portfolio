600af6db0e1c4abd408be43a2c1ec35cd9238ec7
manoj-mak <mm98955@gmail.com>
2023-07-02 19:29:39 +0530

    initial

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/newroom1.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/kda.mp4",
+    },
+];

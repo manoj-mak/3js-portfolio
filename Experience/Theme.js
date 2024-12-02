d31888a3f0f5c6ff2d4b8bf52b4c538e14e159ee
manoj-mak <mm98955@gmail.com>
2024-12-02 15:19:36 +0530

    minor info updates

--- Experience/Theme.js
+++ Experience/Theme.js
@@ -1,26 +1,26 @@
 import { EventEmitter } from "events";
 
 export default class Theme extends EventEmitter {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.theme = "light";
+    this.theme = "light";
 
-        this.toggleButton = document.querySelector(".toggle-button");
-        this.toggleCircle = document.querySelector(".toggle-circle");
+    this.toggleButton = document.querySelector(".toggle-button");
+    this.toggleCircle = document.querySelector(".toggle-circle");
 
-        this.setEventListeners();
-    }
+    this.setEventListeners();
+  }
 
-    setEventListeners() {
-        this.toggleButton.addEventListener("click", () => {
-            this.toggleCircle.classList.toggle("slide");
-            this.theme = this.theme === "light" ? "dark" : "light";
-            document.body.classList.toggle("dark-theme");
-            document.body.classList.toggle("light-theme");
-            // console.log(this.theme);
+  setEventListeners() {
+    this.toggleButton.addEventListener("click", () => {
+      this.toggleCircle.classList.toggle("slide");
+      this.theme = this.theme === "light" ? "dark" : "light";
+      document.body.classList.toggle("dark-theme");
+      document.body.classList.toggle("light-theme");
+      // console.log(this.theme);
 
-            this.emit("switch", this.theme);
-        });
-    }
+      this.emit("switch", this.theme);
+    });
+  }
 }
